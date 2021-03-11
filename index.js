527b5a8666fed816ecc1f4161d66d1f9a6e871d0
Dario D'Amico <ddamico@esri.com>
2021-03-11 00:49:41 -0800

    First commit.

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+const port = process.env["PORT"];
+
+app.listen(port);
